1202331a8785d479352f0c6a7cb7b05bfd62d67e
skstlenovo <116804212+skstlenovo@users.noreply.github.com>
2023-05-20 23:04:33 +0530

    commit

---
+++ src/main/java/com/driver/StudentRepository.java
@@ -0,0 +1,74 @@
+package com.driver;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+@Repository
+public class StudentRepository {
+
+    HashMap<String,Student> studentHashMap=new HashMap<>();
+    HashMap<String,Teacher> teacherHashMap=new HashMap<>();
+    HashMap<String, List<String>> pairHashMap=new HashMap<>();
+
+    public void addStudent(Student student){
+        studentHashMap.put(student.getName(),student);
+    }
+
+    public void addTeacher(Teacher teacher){
+        teacherHashMap.put(teacher.getName(),teacher);
+    }
+
+    public void addStudentTeacher(String student,String teacher)
+    {
+        if(pairHashMap.containsKey(teacher))
+        {
+            pairHashMap.get(teacher).add(teacher);
+        }
+        else{
+            List<String> list=new ArrayList<>();
+            list.add(student);
+            pairHashMap.put(teacher,list);
+        }
+    }
+    public Student getStudentByName(String name){
+        return studentHashMap.get(name);
+    }
+
+    public Teacher getTeacherByName(String name){
+        return teacherHashMap.get(name);
+    }
+
+    public List<String> getStudentsByTeacherName(String name)
+    {
+        return pairHashMap.get(name);
+    }
+
+    public List<String> getAllStudents(){
+        List<String> list=new ArrayList<>();
+        for(String s: studentHashMap.keySet())
+        {
+            list.add(s);
+        }
+        return list;
+    }
+
+    public void deleteTeacherByName(String name)
+    {
+        List<String> studentlist=pairHashMap.get(name);
+        for(String i:studentlist)
+        {
+            studentHashMap.remove(i);
+        }
+        pairHashMap.remove(name);
+        teacherHashMap.remove(name);
+    }
+    public void deleteAllTeachers(){
+        for(String teacherName:pairHashMap.keySet())
+        {
+            deleteTeacherByName(teacherName);
+        }
+    }
+}
---
+++ src/main/java/com/driver/StudentService.java
@@ -0,0 +1,54 @@
+package com.driver;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    @Autowired
+    StudentRepository studentRepository;
+
+    public void addTeacher(Teacher teacher){
+        studentRepository.addTeacher(teacher);
+    }
+
+    public void addStudent(Student student){
+        studentRepository.addStudent(student);
+    }
+
+    public void addStudentTeacher(String student,String teacher){
+        studentRepository.addStudentTeacher(student,teacher);
+    }
+
+    public Student getStudentByName(String name){
+        return studentRepository.getStudentByName(name);
+    }
+
+    public Teacher getTeacherByName(String name){
+        return studentRepository.getTeacherByName(name);
+    }
+
+    public List<String> getStudentsByTeacherName(String name)
+    {
+        return studentRepository.getStudentsByTeacherName(name);
+    }
+
+    public List<String> getAllStudents(){
+
+        return studentRepository.getAllStudents();
+    }
+
+    public void deleteTeacherByName(String name)
+    {
+        studentRepository.deleteTeacherByName(name);
+    }
+    public void deleteAllTeachers(){
+
+        studentRepository.deleteAllTeachers();
+    }
+
+}
